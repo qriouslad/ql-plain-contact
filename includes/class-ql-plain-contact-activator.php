fdddf99abbd2185efebfbaa0550ec57eb9cb2a95
Bowo <wibowosulistio@gmail.com>
2019-03-25 20:43:18 +0700

    Add WPPB scaffolding

---
+++ includes/class-ql-plain-contact-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://bowo.io
+ * @since      1.0.0
+ *
+ * @package    Ql_Plain_Contact
+ * @subpackage Ql_Plain_Contact/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Ql_Plain_Contact
+ * @subpackage Ql_Plain_Contact/includes
+ * @author     Bowo <hello@bowo.io>
+ */
+class Ql_Plain_Contact_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
